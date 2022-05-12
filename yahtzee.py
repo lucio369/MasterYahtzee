285529712bfd68d16fa91d562ab84af5dc45efc0
lucio369 <lucasavison@outlook.com>
2022-05-12 22:15:11 +0100

    Major changes in logic inline with vision but mentions attribute lacking in ext.commands. Logic in place, however code not functioning

--- yahtzee.py
+++ yahtzee.py
@@ -4,19 +4,19 @@ from discord.ext import commands
 
 from random import randint, choice, shuffle
 from time import sleep
-
+from itertools import cycle
 
 class Player:
-    def __init__(self, ID):  # init function for Player
+    def __init__(self):  # init function for Player
         # ! Addition of dice values
         # " User cannot assign
         # £ Constant score
-        self.ID = ID
         self.scoreCard = {
             '!1': -1,
             '!2': -1,
             '!3': -1,
             '!4': -1,
+
             '!5': -1,
             '!6': -1,
             '"Sum': -1,
@@ -29,6 +29,7 @@ class Player:
             '£Chance': -1,
             '$Yahtzee': -1,
             '"Total Score': -1}
+        self.stage,self.fresh,self.complete=0, True, False
         self.tempConstructor()
         self.rollPrep()
 
@@ -65,9 +66,11 @@ class Player:
     def allocateScore(self, key):  # sets points according to roll conditions
         try:
             if self.scoreCard[key] != -1:
-                return
+                print('used key')
+                return False
         except KeyError:
-            return
+            print('bad key')
+            return False
         if key[:1] == '!':  # 1-6
             total = 0
             for v in self.rolls:
@@ -116,8 +119,9 @@ class Player:
                     self.scoreCard[key] = score
             elif key[1:] == 'Total Score':
                 if -1 not in list(self.scoreCard.values())[6:-1]:
-                    self.scoreCard[key] = sum(
-                        list(self.scoreCard.values())[6:-1])
+                    self.scoreCard[key] = sum(list(self.scoreCard.values())[6:-1])
+                    return True
+                return False
 
     def roll(self, *args):  # rolls specific (or not) dice to be rolled
         if len(args) > 0:
@@ -141,176 +145,121 @@ class Player:
             outMsg = outMsg+i+' '+str(self.scoreCard[i])+'\n'
         return outMsg
 
+    def anlsRoll(self,msg): # analyses roll to check if input is appropriate (msg formatted as list of stripped string 1.b
+        #if len(msg) == 0:return True
+        try:
+            for index in msg:
+                if not int(index) in [x for x in range(5)]:
+                    print('That isn\'t an available dice')
+                    raise anlsErr
+        except ValueError:
+            print('That input isn\'t numeric')
+            raise anlsErr
+        except anlsErr:
+            return False
+        else:
+            return True
 
-def game(objList, playerIndex, topPlayer, stage, fresh, raw_input, ignoreList):
-    if stage == 0:
-        if fresh is True:
-            # output player's score card
-            print('Player {}\n\n{}'.format(
-                playerIndex+1, objList[playerIndex].outScore()))
-            objList[playerIndex].roll()  # initial roll
-            # output rolls and have users input what they want to change
-            rMessage = '\n{}Enter the indexes of rolls you wish to change:\n'
-            print(rMessage.format(objList[playerIndex].rolls))
-            return objList, playerIndex, topPlayer, 0, False, ignoreList
-        quickLoop = True
-        cutlist = list(raw_input.strip())  # ignore the rest (we're cool)
-        if len(cutlist) == 0:
-            quickLoop = False
-            return objList, playerIndex, topPlayer, 1, True, ignoreList
+    def chckScore(self,msg):    # allocating appropriate score according to scorecard 2.a.ii
+        try:
+            if msg not in self.scoreCard.keys():
+                print('Inappropriate key')
+                raise anlsErr
+            elif self.scoreCard[msg] != -1:
+                print('Points already allocated here')
+                raise anlsErr
+        except anlsErr:
+            self.reqAllocate()
+            return False
+        else:
+            self.allocateScore(msg)
+            return True
 
-        if quickLoop:
-            for index in cutlist:  # check if user input appropriate integers
-                try:
-                    rollError = True
-                    if not int(index) in [x for x in range(5)]:
-                        print("That's not 0-4")
-                        break
-                    rollError = False
-                except ValueError:
-                    print("That's not even an integer")
-                    break
-            if rollError is True:
-                print('Enter the indexes of rolls you wish to change:\n')
-                return objList, playerIndex, topPlayer, 0, False, ignoreList
-
-        # roll accordingly and check if any rolls remaining if required
-        quickLoop = objList[playerIndex].roll(cutlist)
-        print('\n\n{}Enter the indexes of rolls you wish to change:\n'.format(
-            objList[playerIndex].rolls))
-
-        if quickLoop:
-            return objList, playerIndex, topPlayer, 0, False, ignoreList
-        return objList, playerIndex, topPlayer, 1, True, ignoreList
-    elif stage == 1:
-        if fresh is True:
-            print('Enter the key of the dictionary item you want to change:')
-            return objList, playerIndex, topPlayer, 1, False, ignoreList
-            tOutput = ''
-        if raw_input not in objList[playerIndex].scoreCard.keys():
-            tOutput = '''
-            Inappropriate key
-            Enter the key of the dictionary item you want to change:
-            '''
-        elif objList[playerIndex].scoreCard[raw_input] != -1:
-            tOutput = '''
-            Inappropriate key
-            Enter the key of the dictionary item you want to change:
-            '''
-        if tOutput != '':
-            print(tOutput)
-            return objList, playerIndex, topPlayer, 1, False, ignoreList
-
-        objList[playerIndex].allocateScore(raw_input)  # score allocation
-
-        # checks if sum and bonus can be auto-filled
+    def autoAllocate(self): # allocate further points where possible 2.b
         objList[playerIndex].allocateScore('"Sum')
         objList[playerIndex].allocateScore('"Bonus')
-
-        objList[playerIndex].rollPrep()  # resets dice for next round
-
-        # checks if scorecard is complete
-        objList[playerIndex].allocateScore('"Total Score')
-        if objList[playerIndex].scoreCard['"Total Score'] != -1:
-            if topPlayer[1] < objList[playerIndex].scoreCard['"Total Score']:
-                sCard = objList[playerIndex].scoreCard
-                topPlayer = [playerIndex+1, sCard['"Total Score']]
-            ignoreList.append(playerIndex)
-
-        playerIndex = playerIndex+1  # changes player (if possible)
-        if playerIndex not in range(0, len(objList)):
-            if len(objList) == 0:
-                topPlayer = tuple(topPlayer)
-                return objList, playerIndex, topPlayer, 0, True, ignoreList
-            playerIndex = 0
-        return objList, playerIndex, topPlayer, 0, True, ignoreList
+        return objList[playerIndex].allocateScore('"Total Score')
 
 
-def main():
-    gameLoopFlag = True
-    players = 2
-    oL = [Player(x) for x in range(players)]
-    pI = 0
-    tP = [0, 0]
-    s = 0
-    f = True
-    rI = ''
-    iL = []
+def game(player,msg):
+    '''
+    Stage 1:
+    a. awaiting input for rolls to change
+    b. analysing rolls and checking for remaining rolls (prompting input if has more rolls)
+    Stage 2:
+    a. awaiting input to determine what to allocate the roll to
+       allocating appropriate score according to validity of rolls according to scorecard
+    b. Score allocation
+    '''
 
-    indexScript = []
-    actionScript = [x for x in oL[0].scoreCard.keys() if '"' not in x]
-    for i in range(len(actionScript)):
-        temp = ''
-        templ = [x for x in range(5)]
-        for j in range(randint(0, 5)):
-            tempi = choice(templ)
-            temp = temp+'{}'.format(tempi)
-            templ.remove(tempi)
-        indexScript.append(temp)
-        if temp != '':
-            indexScript.append('')
-    shuffle(indexScript)
-    shuffle(actionScript)
-    indexScript = indexScript*players
-    actionScript = actionScript*players
-    print('\n\n{}\n\n{}'.format(indexScript, actionScript))
-    indexScript = iter(indexScript)
-    actionScript = iter(actionScript)
+    if stage == 0:
+        if player.fresh == True:
+            print(f'Input the rolls you wish to change```{player.rolls}```')
+            player.fresh=False
+        else:
+            player.fresh=True
+            if player.anlsRoll(msg):
+                player.stage=1
+    else:
+        if player.fresh == True:
+            print(f'{player.outScore()}\n\nEnter the key of the dictionary item you wish to change')
+        else:
+            player.fresh=True
+            if player.chckScore(msg):
+                player.stage=0
+                if player.autoAllocate():
+                    player.complete = True
+                    return True
 
-    while gameLoopFlag:
-        if pI in iL:
-            if len(iL) == players:
-                gameLoopFlag = False
-                break
-            else:
-                while pI in iL:
-                    pI = pI+1
-                    if pI not in range(0, players):
-                        pI = 0
-        # objList, playerIndex, topPlayer
-        oL, pI, tP, s, f, iL = game(oL, pI, tP, s, f, rI, iL)
-        if type(tP) is tuple:
-            gameLoopFlag = False
-            continue
-        if f is False:
-            # rI=input()
-            # for AI
-            if pI == 0:
-                rI = input()
-                continue
-            if s == 0:
-                rI = next(indexScript)
-            if s == 1:
-                rI = next(actionScript)
-            print(rI)
-            # input()
+def runYahtz(ctx):
+    '''
+    initial parameters (maybe having main and game functions in a class might be useful??)
 
-            # Multiplayer w/ keyboard (uncomment 242 and comment out until 251)
-            # Play with bots as well (leave 244 - 251 uncommented)
-            # Play only with bots (comment out 244 - 246)
+    check if author is still involved (whiteItem)
+    run game for author
+    remove finished authors and add score to leaderboard
+    if no authors remaining, reveal leaderboard
+    '''
 
+    if bot.game(bot.playerDict[ctx.author],ctx.content):
+        out = ctx.author + ':' + bot.playerDict[ctx.author].scoreCard['"Total Score']
+        bot.leaderboard.append(out)
+        del bot.playerDict[ctx.author]
+        if not bot.playerDict:
+            bot.gaming = False
 
 load_dotenv()  # reads bot token
 TOKEN = os.getenv('DISCORD_TOKEN')
+
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')  # creates discord client and runs it
-
 
 @bot.event
 async def on_ready():
     print(bot.user, 'is connected')  # status message for console
 
+    bot.gaming = False
 
 @bot.listen()
 async def on_message(ctx):
+    print(ctx.content)
     if ctx.author == bot.user:
         return
 
+    if bot.gaming:
+        if ctx.author in bot.playerDict:
+            await bot.runYahtz()
+
 
 @bot.command()
-async def x(ctx, *apple):
-    await ctx.send('output')
+async def yahtzee(ctx):
+    bot.playerDict = dict()
+    for x in ctx.mentions:
+        bot.playerDict[f'{x}'] = Player()
+    bot.leaderboard = []
+    bot.gaming = True
+    await bot.runYahtz()
 
 if __name__ == '__main__':  # only run if not imported
     bot.run(TOKEN)
